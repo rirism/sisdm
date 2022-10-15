2d13f6063b5b8d679c14c2ba764175cc40e73353
Riris Melissa <rirismelissawinonatogatorop@Ririss-MacBook-Pro-2.local>
2022-10-16 00:27:45 +0700

    menambahkan file readme dan gitignore

---
+++ sisdm/src/test/java/proyek/sisdm/SisdmApplicationTests.java
@@ -0,0 +1,13 @@
+package proyek.sisdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
